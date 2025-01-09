43f8574964fd9f2bdfaca1cb55fc83e4e6d472e1
JoaoACastro <joaoaccastro@gmail.com>
2025-01-09 14:38:40 -0300

    fixed error github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'build', // Define a pasta de saída como 'build'
+  },
+  base: '/react-todoList/', // Necessário para o GitHub Pages
+});
